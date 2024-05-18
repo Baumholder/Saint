01a62f0fbb4a57d5ade3e2d53d7d8ab29fb3042f
Baumholder <therod808@vt.edu>
2024-05-17 20:29:55 -0400

    Finished Database

--- Src/Logic/Database.cs
+++ Src/Logic/Database.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Data.Common;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Saints.Logic
 {
@@ -8,13 +6,13 @@ namespace Saints.Logic
     {
         //list of possible searchable attributes
         //_data stores the saint class
-        private Dictionary<int, SaintCard> _data;
-        private Dictionary<string, HashSet<int>> _name;
-        private Dictionary<string, HashSet<int>> _traits;
-        private Dictionary<string, HashSet<int>> _virtues;
+        private readonly Dictionary<int, SaintCard> _data;
+        private readonly Dictionary<string, HashSet<int>> _name;
+        private readonly Dictionary<string, HashSet<int>> _traits;
+        private readonly Dictionary<string, HashSet<int>> _virtues;
         private Dictionary<string, HashSet<int>> _patron;
         private Dictionary<string, HashSet<int>> _titles;
-        private int Index;
+        private int _index;
         
             
         //Constructor 
@@ -22,27 +20,175 @@ namespace Saints.Logic
         //todo add logic
         public Database()
         {
+            //Initializes the variables
             _data = new Dictionary<int, SaintCard>();
             _name = new Dictionary<string, HashSet<int>>();
             _traits = new Dictionary<string, HashSet<int>>();
             _virtues = new Dictionary<string, HashSet<int>>();
             _patron = new Dictionary<string, HashSet<int>>();
             _titles = new Dictionary<string, HashSet<int>>();
-            Index = 0;
-        }
+            _index = 0;
+            
+            //Adds allowed Traits & Virtues
+            HashSet<int> adder = new HashSet<int>();
+            AddTraits(adder);
+            AddVirtues(adder);
+            //TODO above
 
-        //Updates the list of saints
-        //Takes in the info for one saint and adds them to the database
-        //Don't forget to delete saint card before updating if applicable
-        //!!!!!Should only be used at the launch of the program!!!!!
-        public void Update(SaintCard saint)
-        {
-            _data.Add(Index, saint);
-            
-            
-            
-            Index++;
         }
         
+        //Adds the preset Traits
+        private void AddTraits(HashSet<int> adder)
+        {
+            string[] traits = new string[] { "Martyr", "Apostle", "Evangelist", "Doctor", "Virgin", "Monastic", "Hermit",
+                "Pope", "Clergy", "Noble", "VirginMartyr", "Social", "Mystic", "Missionary", "Military", "Layperson",
+                "Child", "Woman", "Man", "Local", "Unknown", "Mythical"};
+            foreach (string i in traits)
+            {
+                _traits.Add(i, new HashSet<int>(adder));
+            }
+        }
+        
+        //adds the preset Virtues
+        private void AddVirtues(HashSet<int> adder)
+        {
+            string[] virtues = new string[] { "Prudence", "Justice", "Fortitude", "Temperance", "Faith", "Hope",
+                "Charity", "Chastity", "Good Works", "Concord", "Sobriety", "Patience", "Humility", "Diligence",
+                "Kindness"};
+            foreach (string i in virtues)
+            {
+                _virtues.Add(i, new HashSet<int>(adder));
+            }
+        }
+
+        //Adds to the list of saints
+        //Takes in the info for one saint and adds them to the database
+        //Does not detect repeats of already added saints
+        //!!!!!Should only be used at the launch of the program!!!!!
+        public void Add(SaintCard saint)
+        {
+            //Initial Variables
+            //They called here and then recycled to help memory allocation by non constantly making a new var.
+            HashSet<int> adder = new HashSet<int>();
+            int count;
+            int index = _index;
+            
+            //Adds Saint Object
+            _data.Add(index, saint);
+            
+            //Adds Saint's Names + Nicknames. Does allow repeated names. All 700 Saint Marys will be included xD.
+            NameAdd(adder, saint.Name, index);
+            
+            //Adds nicknames if applicable
+            count = saint.Nicknames.Count;
+            if (count > 0) {
+                foreach (string nickname in saint.Nicknames) {
+                    NameAdd(adder, nickname, index);
+                }
+            }
+            
+            //Adds Saint's Traits. Note all possible traits are already defined.
+            count = saint.Traits.Count;
+            if (count > 0) {
+                foreach (string traits in saint.Traits) {
+                    _traits[traits].Add(index);
+                }
+            }
+            
+            //Adds Saint's Virtues. Note all possible traits are already defined.
+            count = saint.Virtues.Count;
+            if (count > 0) {
+                foreach (string virtues in saint.Virtues) {
+                    _virtues[virtues].Add(index);
+                }
+            }
+            
+            //Adds Saint's Patronages
+            count = saint.Patron.Count;
+            if (count > 0) {
+                foreach (string patron in saint.Patron) {
+                    PatronAdd(adder, patron, index);
+                }
+            }
+            
+            //Adds Saint's Titles
+            count = saint.Titles.Count;
+            if (count > 0) {
+                foreach (string titles in saint.Titles) {
+                    TitleAdd(adder, titles, index);
+                }
+            }
+            
+            _index++;
+        }
+        
+        //code to add Saint's names + nicknames to database
+        private void NameAdd(HashSet<int> adder, string name, int index)
+        {
+            //checks if saint name is already in the list, creating a new hashset if false.
+            if (!_name.ContainsKey(name)) {
+                adder.Add(index);
+                _name.Add(name, new HashSet<int>(adder));
+                adder.Clear();
+            }
+            //code for a Saint name already used. Repeats are allowed
+            else {
+                _name[name].Add(index);
+            }
+        }
+        
+        //code to add Saint's patronage to database
+        private void PatronAdd(HashSet<int> adder, string name, int index)
+        {
+            //checks if saint patronage is already in the list, creating a new hashset if false.
+            if (!_patron.ContainsKey(name)) {
+                adder.Add(index);
+                _patron.Add(name, new HashSet<int>(adder));
+                adder.Clear();
+            }
+            //code for a Saint patronage already used. Repeats are allowed
+            else {
+                _patron[name].Add(index);
+            }
+        }
+        
+        //code to add Saint's titles to database
+        private void TitleAdd(HashSet<int> adder, string name, int index)
+        {
+            //checks if saint name is already in the list, creating a new hashset if false.
+            if (!_titles.ContainsKey(name)) {
+                adder.Add(index);
+                _titles.Add(name, new HashSet<int>(adder));
+                adder.Clear();
+            }
+            //code for a Saint name already used. Repeats are allowed
+            else {
+                _titles[name].Add(index);
+            }
+        }
+        
+        //As name implies
+        public int GetSaintCount()
+        {
+            return _index;
+        }
+        
+        //Short for "Get Saint With Index"
+        //As name implies
+        public SaintCard GetSaintWIndex(int index)
+        {
+            return _data[index];
+        }
+        
+        //Any Command below here is used for testing
+        public HashSet<int> TestNames(string testName)
+        {
+            return _name[testName];
+        }
+
+        public List<string> KeyList() //todo temp
+        {
+            return new List<string>(_name.Keys);
+        }
     }
 }
--- Src/Logic/Test.cs
+++ Src/Logic/Test.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Saints.Logic
 {
     public class Test
     {
         public Test()
         {
+            Console.Write(Environment.NewLine);
             SaintCardTest();
+            Console.Write(Environment.NewLine);
+            DatabaseTest();
         }
         
         //Test for the SaintCard Class
-        public void SaintCardTest()
+        private void SaintCardTest()
         {
             string[] traits = new string[] {"Martyr", "Man"};
             string[] virtues = new string[] {"Prudence"};
@@ -22,13 +28,85 @@ namespace Saints.Logic
             if (tester1.Name != "Tester1")
             {
                 Console.WriteLine("Expected Name: Tester1, got name: {0}", tester1.Name);
+                return;
             }
-            else
+            else if (tester1.Patron.Count != 0)
             {
-                Console.Write("All Saint Card Tests Passed");
+                Console.WriteLine("Expected Patron Count: 0, got Patron count: {0}", tester1.Patron.Count);
+                return;
             }
+            else if (tester2.Patron[0] != "Testing")
+            {
+                Console.WriteLine("Expected Patron: Testing, got Patron: {0}", tester2.Patron[0]);
+                return;
+            }
+            else if (tester2.Titles[1] != "Baron")
+            {
+                Console.WriteLine("Expected Second Title: Baron, got Second Title: {0}", tester2.Titles[1]);
+                return;
+            }
+            
+            Console.Write("All Saint Card Tests Passed");
         }
+        
+        //Tests for the Database Class
+        private void DatabaseTest()
+        {
+            string[] traits = new string[] {"Martyr", "Man"};
+            string[] virtues = new string[] {"Prudence"};
+            SaintCard tester1 = new SaintCard("Tester", "c/", traits, virtues);
+            string[] patron = new string[] {"Testing"};
+            string[] titles = new string[] {"Cardinal", "Baron"};
+            string[] nicknames = new string[] { "Test Dummy" };
+            SaintCard tester2 = new SaintCard("Tester", "c/", traits, virtues, patron, titles, nicknames);
+            Database database = new Database();
+            database.Add(tester1);
+            
+            if (database.GetSaintCount() != 1)
+            {
+                Console.WriteLine("Expected item count of 1, got count: {0}", database.GetSaintCount());
+                return;
+            }
+            if (database.GetSaintWIndex(0) != tester1)
+            {
+                Console.WriteLine("Expected tester1 SaintCard");
+                return;
+            }
+            //tests if names were added correctly
+            HashSet<int> testHash = database.TestNames("Tester");
+            bool contains = testHash.Contains(0);
+            int count = testHash.Count();
+            if (!contains || count != 1)
+            {
+                Console.WriteLine("Incorrect name dictionary. Contains index: {0}, size: {1}", contains, count);
+                return;
+            }
+            
+            
+            //adds second card
+            database.Add(tester2);
+            
+            //tests nicknames being added to the name
+            testHash = database.TestNames("Test Dummy");
+            contains = testHash.Contains(1);
+            count = testHash.Count();
+            if (!contains || count != 1)
+            {
+                Console.WriteLine("Incorrect nickname dictionary. Contains index: {0}, size: {1}", contains, count);
+                return;
+            }
+            
+            //tests for multiple of the same name
+            testHash = database.TestNames("Tester");
+            contains = testHash.Contains(0) & testHash.Contains(1);
+            count = testHash.Count();
+            if (!contains || count != 2)
+            {
+                Console.WriteLine("Incorrect multi-name dictionary. Contains index: {0}, size: {1}", contains, count);
+                return;
+            }
+            
+            Console.Write("All Database Tests Passed");
+        } 
     }
-    
-    //Tests for the Database Class
 }
